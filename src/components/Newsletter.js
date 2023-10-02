3c088e502708ff42d060e5a6cd389f73aa0e4e08
BasharAlessa <eng.bashar.alessa@gmail.com>
2023-10-02 20:43:40 +0200

    first commit

---
+++ src/components/Newsletter.js
@@ -0,0 +1,38 @@
+import { useState, useEffect } from "react";
+import { Col, Row, Alert } from "react-bootstrap";
+
+export const Newsletter = ({ status, message, onValidated }) => {
+  const [email, setEmail] = useState('');
+
+  useEffect(() => {
+    if (status === 'success') clearFields();
+  }, [status])
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    email &&
+    email.indexOf("@") > -1 &&
+    onValidated({
+      EMAIL: email
+    })
+  }
+
+  const clearFields = () => {
+    setEmail('');
+  }
+
+  return (
+      <Col lg={12}>
+        <div className="newsletter-bx wow slideInUp">
+          <Row>
+            <Col lg={12} md={6} xl={5}>
+              <h3>In-Posts</h3>
+            </Col>
+            <Col md={6} xl={7}>
+              <h4>here we need to add the posts</h4>
+            </Col>
+          </Row>
+        </div>
+      </Col>
+  )
+}
---
+++ src/components/Projects.js
@@ -0,0 +1,100 @@
+import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import { ProjectCard } from "./ProjectCard";
+import projImg1 from "../assets/img/refu'in (1).jpg";
+import projImg2 from "../assets/img/refu'in 3.jpg";
+import projImg3 from "../assets/img/refu'in (2).jpg";
+import colorSharp2 from "../assets/img/color-sharp2.png";
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+
+export const Projects = () => {
+
+  const projects = [
+    {
+      title: "Dutch-In",
+      description: "Education is the key that unlocks the doors to a world of endless possibilities.  `Refu'in`",
+      imgUrl: projImg1,
+    },
+    {
+      title: "Hands-In",
+      description: "Equality work begins with active participation, where every voice is heard and every hand contributes to building a more just world.  `Refu'in`",
+      imgUrl: projImg2,
+    },
+    {
+      title: "Value-In",
+      description: "Your dedication and hard work are truly valuable to us.  `Refu'in`",
+      imgUrl: projImg3,
+    },
+    // {
+    //   title: "Business Startup",
+    //   description: "Design & Development",
+    //   imgUrl: projImg1,
+    // },
+    // {
+    //   title: "Business Startup",
+    //   description: "Design & Development",
+    //   imgUrl: projImg2,
+    // },
+    // {
+    //   title: "Business Startup",
+    //   description: "Design & Development",
+    //   imgUrl: projImg3,
+    // },
+  ];
+
+  return (
+    <section className="project" id="projects">
+      <Container>
+        <Row>
+          <Col size={12}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+              <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
+                <h2>My In-Posts</h2>
+                {/* <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p> */}
+                <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                    <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                      <Nav.Item>
+                        <Nav.Link eventKey="first">Post 1</Nav.Link>
+                      </Nav.Item>
+                      <Nav.Item>
+                        <Nav.Link eventKey="second">Post 2</Nav.Link>
+                      </Nav.Item>
+                      <Nav.Item>
+                        <Nav.Link eventKey="third">Post 3</Nav.Link>
+                      </Nav.Item>
+                    </Nav>
+                  <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
+                    <Tab.Pane eventKey="first">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="second">
+                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="third">
+                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                    </Tab.Pane>
+                  </Tab.Content>
+                </Tab.Container>
+                <Row>
+                        {
+                          projects.map((project, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...project}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
+
+              </div>}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+      <img className="background-image-right" src={colorSharp2}></img>
+    </section>
+  )
+}
