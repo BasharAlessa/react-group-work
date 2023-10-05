35f9e670556c7756011a916627fbcc2d8bd51b58
BasharAlessa <eng.bashar.alessa@gmail.com>
2023-10-05 23:27:09 +0200

    faslt

--- src/components/BannerAbout.js
+++ src/components/BannerAbout.js
@@ -73,7 +73,7 @@ export const BannerAbout = () => {
 
   return (
     <section className="Banner" id="home">
-      <Container>
+      <Container class="con-try">
         <Row className="aligh-items-center">
           <Col xs={12} md={6} xl={7}>
             <TrackVisibility>
@@ -84,23 +84,7 @@ export const BannerAbout = () => {
           {/* event submit  */}
           {/* to delete action  and method  */}
           <br/>
-    <h5>Welcome to <strong>Refu'In</strong> Empowering Refugees Through Employment</h5>
-    <br/>
-    <p>At <strong>Refu'In</strong>, we believe in the power of opportunity and the resilience of the human spirit. We understand that seeking refuge in a new land can be a daunting journey, filled with challenges and uncertainties. 
-        But we also know that refugees bring incredible skills, talents, and a strong work ethic to their new communities.</p>
-    
-    <br/>
-    <h6>Our Mission</h6>
-    <p>o bridge the gap between refugees seeking a fresh start and employers eager to tap into a diverse and talented workforce. We envision a world where every refugee has the chance to rebuild their life, contribute their unique abilities, and thrive in their new homeland.</p>
-    <h6>Our Vision</h6>
-    <p>We envision a world where every refugee has the chance to rebuild their life, contribute their unique abilities, and thrive in their new homeland. We see a future where borders are not barriers to success, and where diversity is not just celebrated but embraced as a catalyst for growth.</p>
-    <h6>Our Goals</h6>
-    <p><ul>
-        <li>Connect Refugees with Meaningful Employment: Our primary goal is to connect refugees with job opportunities that match their skills and aspirations. We aim to facilitate the integration of refugees into the workforce, enabling them to regain their independence and self-sufficiency.</li>
-        <li>Provide Resources for Professional Growth: We are committed to offering resources, training, and support to help refugees develop their professional skills and advance in their careers. Our goal is to empower individuals to reach their full potential.</li>
-        <li>Foster Inclusivity and Diversity: We strive to promote inclusivity and diversity in the workplace by partnering with employers who value the unique perspectives and experiences that refugees bring. Our goal is to create workplaces where everyone feels welcomed and valued.</li>
-        <li>Raise Awareness and Advocate: Beyond job placement, we aim to raise awareness about the challenges faced by refugees and advocate for policies that support their integration and well-being in their new communities.</li>
-    </ul></p>
+   
                 {/* <span className="tagline">Welcome to my In-Profile</span>
                 <h1>{`Hi! i'm Jasem`} <span className="txt-rotate" dataPeriod="3000" data-rotate='[ "painter", "carpenter", "car renter" ]'><span className="wrap">{text}</span></span></h1> */}
                   {/* <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p> */}
@@ -113,7 +97,7 @@ export const BannerAbout = () => {
             <TrackVisibility>
               {({ isVisible }) =>
                 <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
-                  <img src={headerImg} alt="Header Img"/>
+                  {/* <img src={headerImg} alt="Header Img"/> */}
                 </div>}
             </TrackVisibility>
           </Col>
--- src/pages/about.js
+++ src/pages/about.js
@@ -2,14 +2,59 @@ import { NavBar } from '../components/NavBar';
 import {Footer} from "../components/Footer"
 import {BannerAbout } from "../components/BannerAbout"
 import { JasemFooter } from '../components/jasemFooter ';
+import firstpic from "../assets/img/firstpic.jpg"
+
 
 
 const About = () => {
     return (
-<body class="body-about">
+<body >
+
+
 <NavBar />
-<BannerAbout></BannerAbout> 
-<JasemFooter></JasemFooter>
+<div class="about-page"> 
+<br></br>
+<br></br>
+<br></br>
+<br></br>
+<h5>Welcome to <strong>Refu'In</strong> Empowering Refugees Through Employment</h5>
+    <br/>
+    <div class ="about-start">
+    <img class="about-img" src={firstpic} alt="pic"/>
+    
+    <p><br></br>
+<br></br><br></br>
+<br></br>At <strong>Refu'In</strong>, we believe in the power of opportunity and the resilience of the human spirit. <br></br>We understand that seeking refuge in a new land can be a daunting journey, filled with challenges and uncertainties. 
+        <br></br>But we also know that refugees bring incredible skills, talents, and a strong work ethic to their new communities.</p>
+    
+
+    </div>
+    <br/>
+    <div class="m&v-style">
+    <div class="mission-style">
+    <h6>Our Mission</h6>
+    <p>o bridge the gap between refugees seeking a fresh start and employers eager to tap into a diverse and talented workforce. We envision a world where every refugee has the chance to rebuild their life, contribute their unique abilities, and thrive in their new homeland.</p>
+
+    </div>
+    <div class="vision-stule">
+    <h6>Our Vision</h6>
+    <p>We envision a world where every refugee has the chance to rebuild their life, contribute their unique abilities, and thrive in their new homeland. We see a future where borders are not barriers to success, and where diversity is not just celebrated but embraced as a catalyst for growth.</p>
+
+    </div>
+
+    </div>
+
+
+    <h6>Our Goals</h6>
+    <p><ul>
+        <li>Connect Refugees with Meaningful Employment: Our primary goal is to connect refugees with job opportunities that match their skills and aspirations. We aim to facilitate the integration of refugees into the workforce, enabling them to regain their independence and self-sufficiency.</li>
+        <li>Provide Resources for Professional Growth: We are committed to offering resources, training, and support to help refugees develop their professional skills and advance in their careers. Our goal is to empower individuals to reach their full potential.</li>
+        <li>Foster Inclusivity and Diversity: We strive to promote inclusivity and diversity in the workplace by partnering with employers who value the unique perspectives and experiences that refugees bring. Our goal is to create workplaces where everyone feels welcomed and valued.</li>
+        <li>Raise Awareness and Advocate: Beyond job placement, we aim to raise awareness about the challenges faced by refugees and advocate for policies that support their integration and well-being in their new communities.</li>
+    </ul></p>
+</div>
+
+    <JasemFooter></JasemFooter>
 </body>
 
 
