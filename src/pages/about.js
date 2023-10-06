27772b1e5ccbef49fbc430c38f94660fd4b83b8f
BasharAlessa <eng.bashar.alessa@gmail.com>
2023-10-06 11:13:31 +0200

    last and i will not do more

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,7 +6,7 @@ import firstpic from "../assets/img/firstpic.jpg"
 import thirdone from "../assets/img/thirdone.jpg"
 import bridge from "../assets/img/bridge.jpg"
 import succ from "../assets/img/succ.webp"
-
+import fourth from "../assets/img/fourth.png"
 
 const About = () => {
     return (
@@ -33,7 +33,7 @@ At <strong>Refu'In</strong>,
         <br></br>
         we believe in the power of opportunity and the resilience of the human spirit. <br></br>We understand that seeking refuge in a new land can be a daunting journey, filled with challenges and uncertainties. 
         <br></br>But we also know that refugees bring incredible skills, talents, and a strong work ethic to their new communities.</p>
-        <img class="about-img" src={thirdone } alt="pic"/>
+        <img class="about-img-2" src={thirdone } alt="pic"/>
 
 
     </div>
@@ -44,9 +44,10 @@ At <strong>Refu'In</strong>,
 
     <p>Our Mission: bridge the gap between refugees seeking a fresh start and employers eager to tap into a diverse and talented workforce. We envision a world where every refugee has the chance to rebuild their life, contribute their unique abilities, and thrive in their new homeland.</p>
     <img class="about-img-bridge" src={bridge } alt="pic"/>
-
+        
     </div>
     <div class="mission-style">
+
 
     <p>Our Vision:We envision a world where every refugee has the chance to rebuild their life, contribute their unique abilities, and thrive in their new homeland. We see a future where borders are not barriers to success, and where diversity is not just celebrated but embraced as a catalyst for growth.</p>
     <img class="about-img-bridge" src={succ } alt="pic"/>
@@ -59,9 +60,16 @@ At <strong>Refu'In</strong>,
     <h6>Our Goals</h6>
     <p><ul>
         <li>Connect Refugees with Meaningful Employment: Our primary goal is to connect refugees with job opportunities that match their skills and aspirations. We aim to facilitate the integration of refugees into the workforce, enabling them to regain their independence and self-sufficiency.</li>
-        <li>Provide Resources for Professional Growth: We are committed to offering resources, training, and support to help refugees develop their professional skills and advance in their careers. Our goal is to empower individuals to reach their full potential.</li>
+        <div class="about-goal">
+            <div>
+            <li>Provide Resources for Professional Growth: We are committed to offering resources, training, and support to help refugees develop their professional skills and advance in their careers. Our goal is to empower individuals to reach their full potential.</li>
         <li>Foster Inclusivity and Diversity: We strive to promote inclusivity and diversity in the workplace by partnering with employers who value the unique perspectives and experiences that refugees bring. Our goal is to create workplaces where everyone feels welcomed and valued.</li>
         <li>Raise Awareness and Advocate: Beyond job placement, we aim to raise awareness about the challenges faced by refugees and advocate for policies that support their integration and well-being in their new communities.</li>
+
+            </div>
+        <img class="about-img-goal" src={fourth} alt="pic"/>
+
+        </div>
     </ul></p>
 </div>
 
